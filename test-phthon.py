4d27c90ab5ddf42a559456f9a6ca6a350c0e495d
iamdevopstrainer <iamdevopstrainer@gmail.com>
2022-05-30 10:11:38 +0600

    My first phthon file

---
+++ test-phthon.py
@@ -0,0 +1,37 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+print("okay! Lets`s play :) ")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Ïncorrect")
+    
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Ïncorrect")
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Ïncorrect")
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score += 1
+else:
+    print("Ïncorrect")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score/4) * 100) + "%.")
